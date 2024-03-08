dc46c89e4b7a4ec0932bfac8d653f99f2ccb944c
ChouCookieSan <choucookiesan@gmail.com>
2024-03-08 02:39:53 +0100

    Initial commit

---
+++ locales/translations/en.lua
@@ -0,0 +1,5 @@
+Language["en"] = {
+    throwable_interact = "[E] - Prendre l'objet  \n [G] - Donner un coup  \n Puissance : %s",
+    throwable_list = "[E] - Lancer  \n [G] - Laisser tomber  \n [H] - Remettre à la main  \n Puissance : %s",
+    throwable_list_alt = "[E] - Lancer  \n [G] - Laisser tomber  \n [H] - Inventaire  \n Puissance : %s",
+}
